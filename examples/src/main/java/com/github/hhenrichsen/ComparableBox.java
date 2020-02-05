0c6c8bea7d247850b95576fe8ec3a54f99809c7c
Hunter Henrichsen <hunter.henrichsen@gmail.com>
2020-02-04 17:48:33 -0700

    Remove inheritance inside of ComparableBox.

--- examples/src/main/java/com/github/hhenrichsen/ComparableBox.java
+++ examples/src/main/java/com/github/hhenrichsen/ComparableBox.java
@@ -1,13 +1,35 @@
 package com.github.hhenrichsen;
 
-public class ComparableBox extends Box implements Comparable<ComparableBox> {
+public class ComparableBox implements Comparable<ComparableBox> {
+
+    public final int length, width, height;
 
     public ComparableBox(int length, int width, int height) {
-        super(length, width, height);
+        this.length = length;
+        this.width = width;
+        this.height = height;
+    }
+
+    public int calcVolume() {
+        return this.length * this.width * this.height;
+    }
+
+    public int calcSurfaceArea() {
+        return 2 * (length * width) + 2 * (width * height) + 2 * (height * length);
+    }
+
+    @Override
+    public String toString() {
+        return "Box{" +
+                "length=" + length +
+                ", width=" + width +
+                ", height=" + height +
+                ", volume=" + calcVolume() +
+                '}';
     }
 
     @Override
     public int compareTo(ComparableBox other) {
-        return this.calcVolume() - other.calcVolume();
+        return this.calcSurfaceArea() - other.calcSurfaceArea();
     }
 }
